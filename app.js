af8be6245554a9f9e2ce6c1f78a509af95bbf08a
Larry Zhang <larryzhang25@gmail.com>
2014-11-17 04:17:50 -0800

    test to see if data goes is sent

--- app.js
+++ app.js
@@ -22,28 +22,32 @@ app.all('*', function(req, res, next){
 });
 
 app.post('/sendLocation', function(request, response) {
-	var login = request.body.login,
-	lat = request.body.lat,
-	lng = request.body.lng,
-	d = new Date();
-	validator.isNumeric(lat);
-	validator.isNumeric(lng);
-	validator.isAlphanumeric(login);
+	var login = request.body.login;
+	var lat = request.body.lat;
+	var lng = request.body.lng;
+	var d = new Date();
+	//validator.isNumeric(lat);
+	//validator.isNumeric(lng);
+	//validator.isAlphanumeric(login);
 	var toInsert = {
-		"login":login,
-		"lat":lat,
-		"lng":lng,
-		"created_at":d,
+		"login": login,
+		"lat": lat,
+		"lng": lng,
+		"created_at": d
 	};
 	db.locations.insert(toInsert);
-	var JSONstring = '{"characters":[],"students":';
-	db.locations.find().sort({ created_at: 1 });
-	JSONstring += "[";
-	for (var i = 0; i < 100; i++) {
-		JSONstring += cursor[i];
-	}
-	JSONstring += "]}";
-	response.send(JSONstring);	
+	console.log(login);
+//	var JSONstring = '{"characters":[],"students":[';
+//	db.locations.find().sort({ created_at: 1 });
+//	var all = db.locations.find().toArray();
+//	if (all.length>0)
+//		JSONstring += all;
+//	for (var i = 0; i < 100; i++) {
+//		JSONstring += cursor[i];
+//	}
+//	JSONstring += "]}";
+//	response.send(JSONstring);	
+response.send('success');
 });
 
 app.listen(process.env.PORT || 3000);
