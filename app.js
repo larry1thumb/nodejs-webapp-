b0bcea6ae91fa467a6994717a481fd74e8a62d76
Larry Zhang <larryzhang25@gmail.com>
2014-11-16 23:43:15 -0800

    temp remove validator

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Initialization
 var express = require('express');
 var bodyParser = require('body-parser');
-var validator = require('validator');
+//var validator = require('validator');
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
