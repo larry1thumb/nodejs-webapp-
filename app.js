6c18c6e1fc11eaea527d8e7002158473148c95fc
Larry Zhang <larryzhang25@gmail.com>
2014-11-18 23:09:03 -0800

    fixed json part of post

--- app.js
+++ app.js
@@ -10,30 +10,33 @@ var mongoUri = process.env.MONGOLAB_URI ||
 			   process.env.MONGOHQ_URL ||
 			   'mongodb://localhost/assignment3';
 var mongo = require('mongodb');
-var db = mongo.Db.connect(mongoUri, function(error, databaseConnection) {
+var db = mongo.Db.connect(mongoUri, function (error, databaseConnection) {
 	db = databaseConnection;
 });
 
-app.all('*', function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Content-Type');
-	next();
+app.use(function (req, res, next) {
+  	res.header("Access-Control-Allow-Origin", "*");
+  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  	next();
 });
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.set('Content-Type', 'text/html');
   response.send('<p>Hey, it works!</p>');
 });
 
-app.post('/sendLocation', function(request, response) {
+app.post('/sendLocation', function (request, response) {
 	response.header("Access-Control-Allow-Origin", "*");
-	response.header("Access-Control-Allow-Headers", "X-Requested-With");
-	var data = '';
+	response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	response.setHeader("Content-Type", "application/json");
 	var login = request.body.login;
 	var lat = parseFloat(request.body.lat);
 	var lng = parseFloat(request.body.lng);
 	var d = new Date();
+	if (login==undefined || lat==undefined || lng==undefined) {
+		res.send(500);
+	}
+
 	var toInsert = {
 		"login": login,
 		"lat": lat,
@@ -41,7 +44,7 @@ app.post('/sendLocation', function(request, response) {
 		"created_at": d,
 	};
 		
-	db.collection('locations', function(error1, collection) {
+	db.collection('locations', function (error1, collection) {
 		var id = collection.insert(toInsert, function(error2, saved) {
 			if (error2) 
 			{
@@ -49,11 +52,27 @@ app.post('/sendLocation', function(request, response) {
 			}
 			else 
 			{
+				var json = '';
+				collection.find().sort({ created_at: -1 });
 				collection.find().toArray(function(error3, cursor) {
 					if (!error3) 
 					{
-						data = collection.find().sort({ created_at: -1 });
-						var json = JSON.stringify(data);
+						json += "{\"characters\":[],\"students\":[";
+						var number;
+						if (cursor.length - 100 >= 0) {
+							var temp = cursor.length - 100;
+							number = cursor.length - temp;
+						} else {
+							number = cursor.length;
+						}
+						for (var i = number-1; i>=0; i--) {
+							json += JSON.stringify(cursor[i]);
+							if(i>0) {
+								json += ",";
+							}
+							json += "]}";
+						}
+
 						response.send(json);
 					}
 				});
@@ -79,7 +98,6 @@ app.get('/locations.json', function(request, response) {
 });
 
 app.get('/redline.json', function(request, response) {
-	response.setHeader('Content-Type', 'application/json');
 	var http = require('http');
 	var options = {
 		host: 'developer.mbta.com',
@@ -93,6 +111,11 @@ app.get('/redline.json', function(request, response) {
 			data += chunk;
 		});
 		apiresponse.on('end', function() {
+			response.setHeader("Access-Control-Allow-Origin", "*");
+			response.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+			response.setHeader("Content-Type", "application/json");
+			response.send(data);
+		});
 			response.send(data);
 		});
 	}).on('error', function(error) {
